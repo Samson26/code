5a6f7907a0a1794b79a8340da9a979fb2a98638f
A. Samson <47207320+Samson26@users.noreply.github.com>
2019-07-27 14:12:47 +0530

    Create numletter.py

---
+++ numletter.py
@@ -0,0 +1,18 @@
+a=int(input())
+def numlet(b):
+	switcher={
+		0:"Zero",
+		1:"One",
+		2:"Two",
+		3:"Three",
+		4:"Four",
+		5:"Five",
+		6:"Six",
+		7:"Seven",
+		8:"Eight",
+		9:"Nine",
+		10:"Ten",
+	}
+	return switcher.get(b,"nothing")
+	
+print(numlet(a))
